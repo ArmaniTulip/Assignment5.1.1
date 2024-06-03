f5035a31b2954003082901d2f86dde2f9dbdb29b
Garrett <Garrett@YUP>
2024-06-03 13:49:25 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to be tested for being a palindrome");
+
+            string userNums = Console.ReadLine(); 
+
+            bool isPalindrome = IsPalindrome(userNums);
+
+            Console.WriteLine(isPalindrome ? "true" : "false");
+
+        }
+
+        static bool IsPalindrome(string number)
+        {
+            int length = number.Length;
+
+            for (int i = 0; i < length / 2; i++)
+            {
+                if (number[i] != number[length - 1 - i])
+                {
+                    return false;
+
+                }
+            }
+            return true;
+        }
+         
+    }
+}
